9674d47d1564b738c46a86c259cab4f13c41c11d
John <jddhalogamer@gmail.com>
2021-02-09 15:49:25 -0600

    connected to external API

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const path = require('path');
-
+const request = require('request');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('pika pika');
+request('https://pokeapi.co/api/v2/pokemon', (error, response, body) => {
+    console.error('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
 })
+
+
+app.use(express.static(path.join(__dirname, 'public',)));
 
 const port = process.env.port || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => 
+console.log(`Server started on port ${port}`));
+
+module.exports = app;
