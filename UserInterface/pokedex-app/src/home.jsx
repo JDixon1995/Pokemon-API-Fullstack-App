e1b9ed0d6fbb782d27a1956915e99575130a6984
John <jddhalogamer@gmail.com>
2021-02-11 17:44:26 -0600

    wrote function to call information from the API

--- UserInterface/pokedex-app/src/home.jsx
+++ UserInterface/pokedex-app/src/home.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import Search from './components/Search.jsx'
+import { fetchPokemon } from './fetch/getPokemon.js'
 
 const HomePage = () => {
 
-    const getPokemon = (query) => {
-        console.log(query);
+    const [pokemon, setPokemon] = React.useState();
+    const [loading, setLoading] = React.useState(false);
+
+    const getPokemon = async (query) => {
+        setLoading(true);
+        const response = await fetchPokemon(query);
+        const results = await response.json();
+        console.log(results);
+        setPokemon(results);
+        setLoading(false);
     }
     return (
         <div>
-         <Search getPokemon={getPokemon} />   
+         <Search getPokemon={getPokemon} />
+        {!loading && pokemon ? (
+            <div>
+                <h2>{pokemon.name}</h2>
+            </div>
+        ): null}
         </div>
     )
 }
